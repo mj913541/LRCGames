2543d8d5ca1ce3f702ec85664d3bbaac7686fbca
mj913541 <malbrecht3317@gmail.com>
2025-12-27 19:25:46 -0600

    lllll

--- plannerDashboard/dashboard.js
+++ plannerDashboard/dashboard.js
@@ -77,11 +77,11 @@ const SCHEDULE_BY_LETTER_DAY = {
 
 // Everyday AM tasks (06:00)
 const EVERYDAY_AM_TASKS = [
+  "Levothyroxine",
   "Switch Dishwasher",
   "Clean Glasses",
   "Deodorant",
   "Eat Breakfast",
-  "Levothyroxine",
   "Brush teeth",
   "Floss",
   "Get Dressed",
@@ -133,10 +133,18 @@ const WORK_CLOSE_TASKS = [
 // Weekly big rocks
 const DEFAULT_WEEKLY_TASKS = {
   work: [
-    "Plan next week’s LRC lessons",
-    "Update LRCQuest links / quests",
-    "Refresh book displays",
-    "Library communication (newsletters, Seesaw, etc.)",
+    "Next week 1st grade lessons",
+    "Next week 2nd grade lessons",
+    "Next week 3rd grade lessons",
+    "Next week 4th grade lessons",
+    "Next week 5th grade lessons",
+    "Process 5 new books",
+    "5 Book hospital books",
+    "Evaluation Evidence",
+    "Sub plans",
+    "Read-A-Thon Fundraiser",
+    "Ordering supplies",
+    "Decorate 2 shelves to match the genre color sections",
   ],
   home: [
     "Meal plan & grocery list",
