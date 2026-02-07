ac286e1f1ec593b7af3678b2fb98fbe0a64c4ec7
mj913541 <malbrecht3317@gmail.com>
2026-02-07 15:22:04 -0600

    readathon-world

---
+++ readathon-world/js/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDpXoneclJAl5kFr7doJmSlgqoN6teGWzI",
+  authDomain: "lrcquest-3039e.firebaseapp.com",
+  projectId: "lrcquest-3039e",
+  storageBucket: "lrcquest-3039e.firebasestorage.app",
+  messagingSenderId: "72063656342",
+  appId: "1:72063656342:web:e355f9119293b3d953bdb7",
+  measurementId: "G-VRKVK0QWY2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
