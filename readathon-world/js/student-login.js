a09c143787d9c56b380e10f6ac1900deef3bf0b8
mj913541 <malbrecht3317@gmail.com>
2026-02-14 13:00:33 -0600

    dsfsd

--- readathon-world/js/student-login.js
+++ readathon-world/js/student-login.js
@@ -1,4 +1,4 @@
-// ccc js/student-login.js
+// aaa js/student-login.js
 // Grade -> Homeroom -> Name -> PIN -> verifyStudentPin -> redirect
 
 import { auth, functions, db, httpsCallable } from "./firebase.js";
