6924111c0fc238ed12ea75e1598fd96a69e7ab01
mj913541 <malbrecht3317@gmail.com>
2026-03-20 12:25:38 -0500

    titlt

--- readathon-world_Ver2/js/admin-assign.js
+++ readathon-world_Ver2/js/admin-assign.js
@@ -28,8 +28,8 @@ const els = {
   btnSignOut: document.getElementById("btnSignOut"),
   hdr: document.getElementById("hdr"),
 
-  dataTitle: document.getElementById("data-Title"),
-  dataSubtitle: document.getElementById("data-Subtitle"),
+  dataTitle: document.getElementById("data-title"),
+  dataSubtitle: document.getElementById("data-subtitle"),
 
   // Existing direct admin assign fields
   targetUserIdInput: document.getElementById("targetUserIdInput"),
