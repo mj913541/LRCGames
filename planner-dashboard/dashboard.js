cdb16bc714163800c6ee66ae59a33f82746e2eb8
mj913541 <malbrecht3317@gmail.com>
2026-08-07 15:01:07 -0500

    update

--- planner-dashboard/dashboard.js
+++ planner-dashboard/dashboard.js
@@ -1,7 +1,1 @@
 console.log("Dashboard JavaScript is connected!");
-
-fetch("header.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("site-header").innerHTML = data;
-    });
