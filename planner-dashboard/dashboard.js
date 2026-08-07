e8a754fadbaf689ff43507bd23520189fb6db520
mj913541 <malbrecht3317@gmail.com>
2026-08-07 14:56:24 -0500

    sdf

--- planner-dashboard/dashboard.js
+++ planner-dashboard/dashboard.js
@@ -1,1 +1,7 @@
 console.log("Dashboard JavaScript is connected!");
+
+fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("site-header").innerHTML = data;
+    });
